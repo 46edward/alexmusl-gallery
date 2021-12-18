66c653574bb01952a67a2503fea5a5c6bacea8d3
46edward <edward150898@gmail.com>
2021-12-18 22:56:18 +0300

    added script's actions

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,19 @@
 const mobMenuBtn = document.querySelector('.header__hamburger');
 const mobileMenu = document.querySelector('.menu');
+const galleryItems = document.querySelectorAll('.gallery__wrapper');
+const galleryFullScreen = document.querySelector('.gallery__fullscreen');
+const galleryFullScreenImg = document.querySelector('.gallery__fullscreen_img');
 
 mobMenuBtn.addEventListener('click', ()=> {
     mobileMenu.classList.toggle('menu_active');
+});
+
+galleryItems.forEach(item => {
+    item.addEventListener('click', ()=> {
+        galleryFullScreen.classList.toggle('gallery__fullscreen_hide');
+    })
+});
+
+galleryFullScreen.addEventListener('click', () => {
+    galleryFullScreen.classList.add('gallery__fullscreen_hide');
 });
