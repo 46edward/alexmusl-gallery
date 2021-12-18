cd06a3cd5067f0b6214f1d9b6deddbfdb5888476
46edward <edward150898@gmail.com>
2021-12-18 22:00:59 +0300

    main menu fidex

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 const mobMenuBtn = document.querySelector('.header__hamburger');
-const mobileMenu = document.querySelector('.mobile-menu');
+const mobileMenu = document.querySelector('.menu');
 
 mobMenuBtn.addEventListener('click', ()=> {
-    mobileMenu.classList.toggle('mobile-menu_active');
+    mobileMenu.classList.toggle('menu_active');
 });
