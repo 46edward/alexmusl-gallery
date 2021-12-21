03a90bec50ac8aed22425e3eb3fe59f2f6c5e824
46edward <edward150898@gmail.com>
2021-12-21 17:35:12 +0300

    site menu & move to page from menu link

--- src/js/script.js
+++ src/js/script.js
@@ -4,6 +4,22 @@ const mobileMenu = document.querySelector('.menu');
 const galleryItems = document.querySelectorAll('.gallery__wrapper');
 const galleryFullScreen = document.querySelector('.gallery__fullscreen');
 const galleryFullScreenImg = document.querySelector('.gallery__fullscreen_img');
+const menuLinks = document.querySelectorAll('.menu__item');
+const sectionsContent = document.querySelectorAll('section');
+
+menuLinks.forEach((item, key) => {
+    item.addEventListener('click', ()=>{
+        sectionsContent.forEach((section, id) => { 
+            if(section.classList.length > 1){
+                let match = section.classList.toString().match(/\w+\_\w+/); // search active class
+                match.forEach(i => {
+                    section.classList.remove(i);
+                }); // deleted all active classes
+            };
+        });
+        sectionsContent[key].classList.add(`${sectionsContent[key].classList.toString()}_active`);
+    });
+});
 
 hamburgerBtn.addEventListener('click', ()=> {
     mobileMenu.classList.toggle('menu_active');
